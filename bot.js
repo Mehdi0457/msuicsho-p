4613feb3beb5e2ef7f16767bca7b91131851d7da
Mehdi0457 <48065343+Mehdi0457@users.noreply.github.com>
2019-03-10 13:31:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -274,6 +274,8 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
    });
  
  
- 
+ client.on('ready',async () => {
+  client.channels.find(ch => ch.id === "553688427623874565" && ch.type === 'voice').join();
+});
  
 client.login(process.env.BOT_TOKEN);
